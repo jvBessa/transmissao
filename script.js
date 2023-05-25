19692574bf591506c3b87db7573957f2e762cb13
João Victor Bessa <jvbpaula@gmail.com>
2023-05-25 17:29:06 -0300

    dia 2
    Adicionei uma finalização;
    Apos o fim, o usuário é levado para uma outra pagina(ainda por fazer);
    background muda conforme responde;

--- script.js
+++ script.js
@@ -13,39 +13,55 @@ var etapaAtual = document.getElementById('etapa');
 var enigma = document.getElementById('enigma');
 var quiz = document.getElementById('quiz');
 var posQuiz = document.getElementById('posQuiz');
-
-console.log(parseInt(etapaAtual.innerText))
-
+var fundo = document.getElementById('fundo');
+console.log(i)
 function fechaQuiz(etapa){
     if(etapa == 6){
         quiz.style.display = "none";
-        posQuiz.style.display = "inline-block";
+        posQuiz.style.display = "flex";
     }
 }
-
+function mudaAlfaBg(valorAlfa){
+    if(valorAlfa == 1){
+        fundo.style.backgroundColor = "rgba(32, 32, 32, 0.8)"
+    }
+    else if(valorAlfa == 2){
+        fundo.style.backgroundColor = "rgba(32, 32, 32, 0.6)"
+    }
+    else if(valorAlfa == 3){
+        fundo.style.backgroundColor = "rgba(32, 32, 32, 0.4)"
+    }
+    else if(valorAlfa == 4){
+        fundo.style.backgroundColor = "rgba(32, 32, 32, 0.2)"
+    }
+    else if(valorAlfa == 5){
+        fundo.style.backgroundColor = "rgba(32, 32, 32, 0)"
+    }
+    
+}
 function verificaResposta(){
 
     if(etapa < 6){
         if(resposta1.value.toUpperCase() == respostas[i]){
+            
             resposta1.value = "";
             etapa++;
             i++;
             j++;
             etapaAtual.innerText = etapa.toString();
             enigma.innerText = enigmas[j];
-            console.log(parseInt(etapaAtual.innerText))
+            
             fechaQuiz(etapa);
         }
         else{
             resposta1.value = "";
             alert("Resposta errada!")
         }
-    }
-        
+    }    
     else if(etapa == 7){
         quiz.style.display = 'none';
     }
-    
+    mudaAlfaBg(i);
 }
 
 document.addEventListener("keyup", function(event) {
@@ -53,5 +69,9 @@ document.addEventListener("keyup", function(event) {
         verificaResposta();
     }
 });
+
+function apagaFundo(){
+    fundo.style.backgroundColor = "rgba(32, 32, 32, 0.9)"
+}
 
 
